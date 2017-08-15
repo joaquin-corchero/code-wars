c073163cb8346f4cb763d08f85557f9bc0c7449f
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-15 14:11:51 +0100

    Done

--- CodeWarsTests/CodeWarsTests/BuildAPileOfCubes/ASum.cs
+++ CodeWarsTests/CodeWarsTests/BuildAPileOfCubes/ASum.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Linq.Expressions;
 using NUnit.Framework;
 
 namespace CodeWarsTests.BuildAPileOfCubes
 {
     class ASum
     {
-        public static double findNb(double m)
+        public static long findNb(long m)
         {
-            return -1;
+            long sum = 0;
+            long counter = 1;
+            while (sum < m)
+            {
+                sum += Convert.ToInt64(Math.Pow(counter , 3));
+                counter++;
+            }
+
+            return sum == m ? counter - 1 : -1;
         }
     }
 
@@ -22,7 +31,8 @@ namespace CodeWarsTests.BuildAPileOfCubes
         [TestCase(24723578342962, ExpectedResult = -1)]
         [TestCase(135440716410000, ExpectedResult = 4824)]
         [TestCase(40539911473216, ExpectedResult = 3568)]
-        public double FixedTest(double arg)
+        [TestCase(1945148215731347025, ExpectedResult = 52814)]
+        public long FixedTest(long arg)
         {
             return ASum.findNb(arg);
         }
