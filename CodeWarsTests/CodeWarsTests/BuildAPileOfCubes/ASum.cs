5894182fdebb23713a384e25ebd08dbeeea69027
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-15 10:42:14 +0100

    Added Build pile of cubes

---
+++ CodeWarsTests/CodeWarsTests/BuildAPileOfCubes/ASum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWarsTests.BuildAPileOfCubes
+{
+    class ASum
+    {
+        public static double findNb(double m)
+        {
+            return -1;
+        }
+    }
+
+
+
+    [TestFixture]
+    public class ASumTests
+    {
+
+        [Test]
+        [TestCase(4183059834009, ExpectedResult = 2022)]
+        [TestCase(24723578342962, ExpectedResult = -1)]
+        [TestCase(135440716410000, ExpectedResult = 4824)]
+        [TestCase(40539911473216, ExpectedResult = 3568)]
+        public double FixedTest(double arg)
+        {
+            return ASum.findNb(arg);
+        }
+    }
+
+}
