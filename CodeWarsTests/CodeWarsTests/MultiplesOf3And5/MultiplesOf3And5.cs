0c454661c70c7cf658bcb9384768f9ef427e8083
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-16 09:41:21 +0100

    Added multiples of 3 and 5

---
+++ CodeWarsTests/CodeWarsTests/MultiplesOf3And5/MultiplesOf3And5.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CodeWarsTests.MultiplesOf3And5
+{
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            return value < 1 ? 0 : Enumerable.Range(1, value - 1).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, Kata.Solution(10));
+        }
+    }
+}
