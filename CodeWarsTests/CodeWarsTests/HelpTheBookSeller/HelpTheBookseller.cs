f9a761312808f6fb24ee7dbff38576a31c22da81
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-16 13:36:01 +0100

    Added test and readme

---
+++ CodeWarsTests/CodeWarsTests/HelpTheBookSeller/HelpTheBookseller.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CodeWarsTests
+{
+    public class StockList
+    {
+
+        public static string stockSummary(String[] lstOfArt, String[] lstOf1stLetter)
+        {
+            if (lstOfArt.Length == 0)
+                return "";
+
+            var i = lstOf1stLetter
+                .Select(l => new { letter = l, codes = lstOfArt.Where(c=> c.IndexOf(l) == 0) })
+                .Select(l=> new { letter = l.letter, number = SumNumbersFromCodes(l.codes)})
+                .Select(x => $"({x.letter} : {x.number})")
+                .ToList();
+
+            return string.Join(" - ", i);
+        }
+
+        private static int SumNumbersFromCodes(IEnumerable<string> codes)
+        {
+            var total = 0;
+            foreach(var code in codes)
+            {
+                total += Convert.ToInt32(code.Split(' ')[1]);
+            }
+
+            return total;
+        }
+    }
+
+
+    [TestFixture]
+    public class StockListTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            string[] art = new string[] { "ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600" };
+            String[] cd = new String[] { "A", "B" };
+            Assert.AreEqual("(A : 200) - (B : 1140)", StockList.stockSummary(art, cd));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string[] art = new string[] { "ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600" };
+            String[] cd = new String[] { "A", "B" , "Z"};
+            Assert.AreEqual("(A : 200) - (B : 1140) - (Z : 0)", StockList.stockSummary(art, cd));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string[] art = new string[];
+            String[] cd = new String[] { "A", "B", "Z" };
+            Assert.AreEqual("(A : 200) - (B : 1140) - (Z : 0)", StockList.stockSummary(art, cd));
+        }
+    }
+
+
+}
