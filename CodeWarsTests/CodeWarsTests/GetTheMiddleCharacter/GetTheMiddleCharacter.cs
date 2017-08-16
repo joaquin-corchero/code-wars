4eb800cfc9fa87210b905ba59e8008e580738812
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-08-16 09:04:43 +0100

    Added get the middle character kata

---
+++ CodeWarsTests/CodeWarsTests/GetTheMiddleCharacter/GetTheMiddleCharacter.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests.GetTheMiddleCharacter
+{
+    public class Kata
+    {
+        public static string GetMiddle(string s)
+        {
+            var length = s.Length;
+            if(length % 2 == 0)
+            {
+                return s.Substring(length / 2 - 1, 2);
+            }
+            return s.Substring((length - 1) / 2, 1);
+        }
+    }
+
+    [TestFixture]
+    public class GetMiddleTest
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", Kata.GetMiddle("test"));
+            Assert.AreEqual("t", Kata.GetMiddle("testing"));
+            Assert.AreEqual("dd", Kata.GetMiddle("middle"));
+            Assert.AreEqual("A", Kata.GetMiddle("A"));
+        }
+    }
+}
