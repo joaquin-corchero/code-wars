1aa2597464c48e5bb3f2af1784096af29d82f41c
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-09-05 23:08:26 +0100

    Added tests from book

---
+++ CodeWarsTests/CodeWarsTests/ArraysAndStrings/ArrayAndResizableArray.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Should.Fluent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsTests.ArraysAndStrings
+{
+    class ArrayAndResizableArray
+    {
+        public static List<string> Merge(string[] words, string[] more)
+        {
+            var result = new List<string>();
+            result.AddRange(GetList(words));
+            result.AddRange(GetList(more));
+
+            return result;
+        }
+
+        public static IEnumerable<string> GetList(string[] words)
+        {
+            foreach (var item in words)
+                yield return item;
+        }
+    }
+
+    [TestFixture]
+    class ArrayAndResizableArrayTest
+    {
+        [TestCase(new string[] { "a", "b", "c" }, new string[] { "d", "e" }, ExpectedResult = new string[] { "a", "b", "c", "d", "e" })]
+        [TestCase(new string[] { "c" }, new string[] { "d", "e" }, ExpectedResult = new string[] {"c", "d", "e" })]
+        public List<string> A_list_is_created_with_the_two_inputs(string[] one, string[] two)
+        {
+            return ArrayAndResizableArray.Merge(one, two);
+        }
+    }
+}
---
+++ CodeWarsTests/CodeWarsTests/ArraysAndStrings/AuniqueCharactersInString.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests.ArraysAndStrings
+{
+    public class AuniqueCharactersInString
+    {
+        internal static bool IsUnique(string input)
+        {
+            var chars = input.ToCharArray();
+            Array.Sort(chars);
+            for(var i = 1; i < chars.Count(); i ++)
+            {
+                if(chars[i] == chars[i-1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        internal static bool IsUniqueHash(string input)
+        {
+            var hashTable = new Hashtable(input.Length);
+            foreach(var c in input)
+            {
+                if (hashTable.ContainsKey(c))
+                    return false;
+
+                hashTable.Add(c, c);
+            }
+            
+            return true;
+        }
+    }
+
+    [TestFixture]
+    public class AuniqueCharactersInStringTest
+    {
+
+        [TestCase("abfhdjktj", ExpectedResult =false)]
+        [TestCase("qwerty", ExpectedResult = true)]
+        [TestCase("asdfg", ExpectedResult = true)]
+        [TestCase("aaaaaaaaaaaa", ExpectedResult = false)]
+        public bool IsUnique(string input)
+        {
+            return AuniqueCharactersInString.IsUnique(input);
+        }
+
+        [TestCase("abfhdjktj", ExpectedResult = false)]
+        [TestCase("qwerty", ExpectedResult = true)]
+        [TestCase("asdfg", ExpectedResult = true)]
+        [TestCase("aaaaaaaaaaaa", ExpectedResult = false)]
+        public bool IsUniqueHash(string input)
+        {
+            return AuniqueCharactersInString.IsUniqueHash(input);
+        }
+    }
+}
