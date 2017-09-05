7f1c9b8beb4a0bffacd0b27a6ea2511675be53e9
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-09-05 23:19:34 +0100

    Added check permutation

---
+++ CodeWarsTests/CodeWarsTests/ArraysAndStrings/Permutations.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Should.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests.ArraysAndStrings
+{
+    public class Permutations
+    {
+        internal static bool ArePermutations(string one, string two)
+        {
+            if (one.Length != two.Length)
+                return false;
+
+            var aOne = one.ToCharArray();
+            Array.Sort(aOne);
+            var aTwo = two.ToCharArray();
+            Array.Sort(aTwo);
+
+            return Array.Equals(aOne, aTwo);
+        }
+    }
+
+    [TestFixture]
+    public class PermutationsTests
+    {
+        [TestCase("change", "gechan", ExpectedResult = true)]
+        [TestCase("changed", "gechan", ExpectedResult = false)]
+        public bool ArePermutation(string one, string two)
+        { 
+            return Permutations.ArePermutations(one, two);
+        }
+    }
+}
