a1a6d2411ff873389a63c6fce165e3ac2b87040b
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-09-06 21:53:11 +0100

    Solved mix string

--- CodeWarsTests/CodeWarsTests/StringMix/StringMix.cs
+++ CodeWarsTests/CodeWarsTests/StringMix/StringMix.cs
@@ -9,21 +9,55 @@ namespace CodeWarsTests.SmallFuckInterpreter
     {
         public static string Mix(string s1, string s2)
         {
-            var h1 = GetCharCount(s1);
-            var h2 = GetCharCount(s2);
+            var a1 = s1.ToCharArray();
+            var a2 = s2.ToCharArray();
 
-            var matches = GetMatches(h1, h2);
+            var dic = new Dictionary<char, Match>();
+            foreach(var c in a1)
+            {
+                if (Char.IsUpper(c) || !Char.IsLetter(c))
+                    continue;
+                Match value;
+                if(dic.ContainsKey(c) && dic.TryGetValue(c, out value))
+                {
+                    value.OnA++;
+                }else
+                {
+                    dic.Add(c, new Match(c, 1, 0));
+                }
+            }
 
-            var result = matches
-                .OrderByDescending(m => m.MaxNumber)
-                .Select(m=> m.ToString());
+            foreach (var c in a2)
+            {
+                if (Char.IsUpper(c) || !Char.IsLetter(c))
+                    continue;
 
-            return string.Join("/", result);
+                Match value;
+                if (dic.ContainsKey(c) && dic.TryGetValue(c, out value))
+                {
+                    value.OnB++;
+                }
+                else
+                {
+                    dic.Add(c, new Match(c, 0, 1));
+                }
+            }
+
+            var values = dic
+                .Values
+                .Where(v => v.MaxNumber > 1)
+                .OrderByDescending(v => v.MaxNumber)
+                .ThenBy(v=> v.Winner)
+                .ThenBy(v=> v.ToString())
+                .ToList();
+
+            return string.Join("/", values);
+            
         }
 
-        private static List<Matches> GetMatches(Dictionary<char, int> h1, Dictionary<char, int> h2)
+        private static List<Match> GetMatches(Dictionary<char, int> h1, Dictionary<char, int> h2)
         {
-            var matches = new List<Matches>();
+            var matches = new List<Match>();
             foreach (var item in h1)
             {
                 if (!h2.ContainsKey(item.Key))
@@ -32,7 +66,7 @@ namespace CodeWarsTests.SmallFuckInterpreter
                 if (item.Value < 2 && h2[item.Key] < 2)
                     continue;
 
-                matches.Add(new Matches(item.Key, item.Value, h2[item.Key]));
+                matches.Add(new Match(item.Key, item.Value, h2[item.Key]));
             }
             return matches;
         }
@@ -57,17 +91,35 @@ namespace CodeWarsTests.SmallFuckInterpreter
         }
     }
 
-    internal class Matches
+    internal class Match
     {
-        internal int NumberA { get; }
+        internal int OnA { get; set; }
 
-        internal int NumberB { get; }
+        internal int OnB { get; set; }
 
         internal int MaxNumber
         {
             get
             {
-                return NumberA > NumberB ? NumberA : NumberB;
+                return OnA >= OnB ? OnA : OnB;
+            }
+        }
+
+        internal int Winner
+        {
+            get
+            {
+                if (OnA == OnB)
+                {
+                    return 3;
+                }
+
+                if (OnB > OnA)
+                {
+                    return 2;
+                }
+
+                return 1;
             }
         }
 
@@ -75,79 +127,48 @@ namespace CodeWarsTests.SmallFuckInterpreter
         {
             get
             {
-                if (NumberA == NumberB)
+                if (OnA == OnB)
                 {
-                    return "=:";
+                    return "=";
                 }
 
-                if (NumberB > NumberA)
+                if (OnB > OnA)
                 {
-                    return "2:";
+                    return "2";
                 }
-                return "1:";
+
+                return "1";
             }
         }
 
-        internal char Item { get; }
+        internal char TheChar { get; }
 
-        internal Matches(char item, int numberA, int numberB)
+        internal Match(char theChar, int numberA, int numberB)
         {
-            Item = item;
-            NumberA = numberA;
-            NumberB = numberB;
+            TheChar = theChar;
+            OnA = numberA;
+            OnB = numberB;
         }
 
         public override string ToString()
         { 
-            return $"{Prefix}:{new String(Item, MaxNumber)}";
+            return $"{Prefix}:{new String(TheChar, MaxNumber)}";
         }
     }
 
     [TestFixture]
-    public static class MixingTests
+    public class MixingTests
     {
 
-        [Test]
-        public static void test1()
+        [TestCase("Are they here", "yes, they are here", ExpectedResult ="2:eeeee/2:yy/=:hh/=:rr")]
+        [TestCase("looping is fun but dangerous", "less dangerous than coding", ExpectedResult = "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg")]
+        [TestCase(" In many languages", " there's a pair of functions", ExpectedResult = "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")]
+        [TestCase("Lords of the Fallen", "gamekult", ExpectedResult = "1:ee/1:ll/1:oo")]
+        [TestCase("codewars", "codewars", ExpectedResult = "")]
+        [TestCase("A generation must confront the looming ", "codewarrs", ExpectedResult = "1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr")]
+        public string ShouldWork(string a, string b)
         {
-            Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", Mixing.Mix("Are they here", "yes, they are here"));
-        }
-
-        [Test]
-        public static void test2()
-        {
-            Assert.AreEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
-                    Mixing.Mix("looping is fun but dangerous", "less dangerous than coding"));
-        }
-
-
-        [Test]
-        public static void test3()
-        {
-            Assert.AreEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
-                    Mixing.Mix(" In many languages", " there's a pair of functions"));
-        }
-
-
-        [Test]
-        public static void test4()
-        {
-            Assert.AreEqual("1:ee/1:ll/1:oo", Mixing.Mix("Lords of the Fallen", "gamekult"));
-        }
-
-
-        [Test]
-        public static void test5()
-        {
-            Assert.AreEqual("", Mixing.Mix("codewars", "codewars"));
-        }
-
-
-        [Test]
-        public static void test6()
-        {
-            Assert.AreEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
-            Mixing.Mix("A generation must confront the looming ", "codewarrs"));
+            return Mixing.Mix(a, b);
         }
     }
 }
